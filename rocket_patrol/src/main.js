ed88084f3057c03c82832c49523394886b79a3f7
mofthomp <45412519+mofthomp@users.noreply.github.com>
2021-04-06 14:44:50 -0500

    rocket patrol play/menu

--- rocket_patrol/src/main.js
+++ rocket_patrol/src/main.js
@@ -3,6 +3,7 @@ console.log("rock of patrol");
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
