3b7450940c34dba568519bf6e9078d932c78520f
mofthomp <45412519+mofthomp@users.noreply.github.com>
2021-04-07 18:39:47 -0500

    moving background

--- src/main.js
+++ src/main.js
@@ -12,3 +12,4 @@ let game = new Phaser.Game(config);
 //set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+let starSpeed = 4;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,20 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
-    create() {
+    //init(), preload(), create(), update()
+
+    preload() {
+        //load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create() { //back to front
+        //place starfield
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
+
+
         // green UI background rect
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
         // white borders
@@ -11,5 +24,10 @@ class Play extends Phaser.Scene{
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+    }
+
+
+    update() {
+        this.starfield.tilePositionX -= starSpeed;
     }
 }
