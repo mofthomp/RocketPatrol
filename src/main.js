68a856cc8b7f51d1517a1d2c571c5da1e811d1ee
mofthomp <45412519+mofthomp@users.noreply.github.com>
2021-04-07 18:23:58 -0500

    borders

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ let config = {
     height: 480,
     scene: [ Menu, Play ]
 }
+
 let game = new Phaser.Game(config);
+
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
