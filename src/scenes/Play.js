d7e59391e225b09b676e53e5d0b17dc98f16f811
mofthomp <45412519+mofthomp@users.noreply.github.com>
2021-04-09 19:15:37 -0500

    explosion animation implemented

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -76,15 +76,15 @@ class Play extends Phaser.Scene{
         //check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)) {
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)) {
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
@@ -99,5 +99,18 @@ class Play extends Phaser.Scene{
         else {
                 return false;
             }
+    }
+
+    shipExplode(ship) {
+        //temporarily hide ship
+        ship.alpha = 0;
+        // create explosion sprite at ship position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0,0);
+        boom.anims.play('explode');
+        boom.on('animationcomplete', () => {
+            ship.reset();
+            ship.alpha = 1;
+            boom.destroy();
+        });
     }
 }
